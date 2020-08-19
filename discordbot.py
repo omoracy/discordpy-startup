d1732fb9a0907dbfe1d58cfd7f4ad5b73834117d
omoracy <67848774+omoracy@users.noreply.github.com>
2020-08-20 00:50:14 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -18,7 +18,15 @@ async def ping(ctx):
     await ctx.send('pong')
 
 
-
+# メッセージ受信時に動作する処理
+@bot.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
 
 
 bot.run(token)
