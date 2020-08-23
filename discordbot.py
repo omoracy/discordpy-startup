a0c8aa16825c505702371b193d0f85cb04a677f6
omoracy <67848774+omoracy@users.noreply.github.com>
2020-08-23 14:47:21 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,6 +7,23 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
+# コマンドに対応するリストデータを取得する関数を定義
+def get_data(message):
+    command = message.content
+    data_table = {
+        '/members': message.guild.members, # メンバーのリスト
+        '/roles': message.guild.roles, # 役職のリスト
+        '/text_channels': message.guild.text_channels, # テキストチャンネルのリスト
+        '/voice_channels': message.guild.voice_channels, # ボイスチャンネルのリスト
+        '/category_channels': message.guild.categories, # カテゴリチャンネルのリスト
+    }
+    return data_table.get(command, '無効なコマンドです')
+
+# 発言時に実行されるイベントハンドラを定義
+@client.event
+async def on_message(message):
+    # コマンドに対応するデータを取得して表示
+    print(get_data(message))
 
 
 @bot.event
@@ -60,10 +77,6 @@ async def on_message(message):
 
     if re.search("こんにちは", message.content): #もし、こんにちはを含むメッセージで、
         if message.channel.id == 746579828693794926:#かつ、もし、神社チャンネルなら
-            member = message.author
-            ROLE_ID2 = 738998001976082503
-            role_2 = bot.get_roles(ROLE_ID2)
-                await member.add_roles(role_2)
                 await message.channel.send("Good afternoon")
         else:#あるいは、もし神社チャンネルでないなら
             await message.channel.send("ここではコマンドは実施できません") 
