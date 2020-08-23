bdc75b48e1cc7e2288eef7d981df495cbdc1f933
omoracy <67848774+omoracy@users.noreply.github.com>
2020-08-23 17:24:58 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -60,7 +60,7 @@ async def on_message(message):
         if message.channel.id == 746579828693794926:#かつ、もし、神社チャンネルなら
             await message.channel.send("呪文") 
             await message.delete()
-            await member.add_roles(id=738998001976082503)
+            await member.add_roles(738998001976082503)
 
         else:#あるいは、もし神社チャンネルでないなら
             await message.channel.send("ここではコマンドは実施できません") 
