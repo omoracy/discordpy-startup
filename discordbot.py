ff5bc2ac954acff24ee98e90e45217538ef58b0b
omoracy <67848774+omoracy@users.noreply.github.com>
2020-08-23 14:48:57 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,24 +7,6 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-# コマンドに対応するリストデータを取得する関数を定義
-def get_data(message):
-    command = message.content
-    data_table = {
-        '/members': message.guild.members, # メンバーのリスト
-        '/roles': message.guild.roles, # 役職のリスト
-        '/text_channels': message.guild.text_channels, # テキストチャンネルのリスト
-        '/voice_channels': message.guild.voice_channels, # ボイスチャンネルのリスト
-        '/category_channels': message.guild.categories, # カテゴリチャンネルのリスト
-    }
-    return data_table.get(command, '無効なコマンドです')
-
-# 発言時に実行されるイベントハンドラを定義
-@client.event
-async def on_message(message):
-    # コマンドに対応するデータを取得して表示
-    print(get_data(message))
-
 
 @bot.event
 async def on_error(ctx, error):
