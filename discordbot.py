84b5a98e9b8d39949b47731e887d33462b301fb6
omoracy <67848774+omoracy@users.noreply.github.com>
2020-08-23 20:41:31 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -78,8 +78,7 @@ async def on_message(message):
 
 
 
-ID_CHANNEL_README = 746579828693794926 # 該当のチャンネルのID
-ID_ROLE_WELCOME = 738998001976082503 # 付けたい役職のID
+
 
 @client.event
 async def on_raw_reaction_add(payload):
