566b0b229ca5dfc57273a6492ff41a7dbe547bf5
omoracy <67848774+omoracy@users.noreply.github.com>
2020-08-23 14:29:22 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -60,6 +60,9 @@ async def on_message(message):
 
     if re.search("こんにちは", message.content): #もし、こんにちはを含むメッセージで、
         if message.channel.id == 746579828693794926:#かつ、もし、神社チャンネルなら
+            member = message.author
+            var = discord.utils.get(message.guild.roles, name = 738998001976082503)#レジデントを与える
+                await member.add_role(var)
                 await message.channel.send("Good afternoon")
         else:
             await message.channel.send("ここではコマンドは実施できません") #あるいは、もし神社チャンネルでないなら
