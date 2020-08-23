528d1e8026b6ec8fe7d1ae4a6704e33c0b3ecb47
omoracy <67848774+omoracy@users.noreply.github.com>
2020-08-23 15:28:38 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -60,7 +60,7 @@ async def on_message(message):
     if re.search("こんにちは", message.content): #もし、こんにちはを含むメッセージで、
         if message.channel.id == 746579828693794926:#かつ、もし、神社チャンネルなら
             await message.author.add_roles(role) # 上記で取得したロールを付与
-            await message.channel.send("Good afternoon")
+            role = discord.utils.get(message.server.roles, name='resident')
         else:#あるいは、もし神社チャンネルでないなら
             await message.channel.send("ここではコマンドは実施できません") 
 
